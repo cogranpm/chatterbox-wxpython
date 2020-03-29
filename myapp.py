c341b43d22cf7583150ba846b4e300f38d1ae5d5
Paul Martin <mussym@live.com>
2020-03-29 05:47:55 -0500

    adding forms

--- myapp.py
+++ myapp.py
@@ -1,25 +1,26 @@
 import wx
-from mainframe import MainFrame
+from MainFrameImp import  MainFrameImp
 import chatterbox_constants as cc
 from fn_app import load_default_settings
-from SettingsDialogImp import SettingsDialogImp
+
 
 class ChatterboxApp(wx.App):
 
     def __init__(self):
         super().__init__()
-        self.frame = None
-        self.data_directory = None
+        # I don't know why but when these are given initial values
+        # the settings in the OnInit are ignored when using the wx.App.Get() to retrieve instance
+        # in other parts of the application
+        #self.frame = None
+        #self.data_directory = ''
 
     def OnInit(self) -> bool:
+        """ System, Toolkit and WxWidgets fully initialized"""
+        super().OnInit()
         wx.ConfigBase.Set(wx.Config(cc.APPLICATION_NAME))
         self.data_directory = load_default_settings()
-        self.frame = MainFrame(None, "Chatterbox")
+        self.frame = MainFrameImp(None)
         self.frame.Show()
-        with SettingsDialogImp(self.frame, self.data_directory) as dlg:
-            if dlg.ShowModal() == wx.ID_OK:
-                print(dlg.dirty)
-                print(dlg.data_directory)
-
+        self.SetTopWindow(self.frame)
         return True
 
