5ceaabd533ee672a135422268638c2132f2373f7
paul martin <mussym@live.com>
2020-04-03 23:57:25 -0500

    seesaw style ui wrappers

--- playground.py
+++ playground.py
@@ -60,7 +60,6 @@ class TestModel(dv.DataViewIndexListModel):
             # notify the view(s) using this model that it has been removed
             self.RowDeleted(row)
 
-
     def AddRow(self, value):
         # update data structure
         self.data.append(value)
@@ -69,8 +68,6 @@ class TestModel(dv.DataViewIndexListModel):
 
 
 class PyTestModel(dv.PyDataViewModel):
-
-
     """
     this class has a neat way to get the object that is stored
     inside the row of the table without resorting to storing pointers etc
@@ -118,7 +115,6 @@ class PyTestModel(dv.PyDataViewModel):
         return True
 
     def Compare(self, item1, item2, col, ascending):
-        print('compare')
         if not ascending: # swap sort order?
             item2, item1 = item1, item2
         row1 = self.ItemToObject(item1)
@@ -141,14 +137,12 @@ class PlaygroundForm(wx.Dialog):
         wx.Dialog.__init__(self, parent, id=wx.ID_ANY, title=u"Settings", pos=wx.DefaultPosition,
                            size=wx.Size(604, 230), style=wx.DEFAULT_DIALOG_STYLE)
 
-
         self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
 
         # testing out the list control
         self.dvc = dv.DataViewCtrl(self, wx.ID_ANY, style = wx.BORDER_THEME)
-        self.data = [['Peter', '33', '100'], ['Fred', '22', '98']]
+        self.data = [['Peter', '33', '100'], ['Fred', '22', '98'], ['Malcolm', '43', '77']]
         self.model = PyTestModel(self.data)
-        #self.model = TestModel(data)
         self.dvc.AssociateModel(self.model)
         self.model.DecRef()
 
@@ -225,6 +219,8 @@ class PlaygroundForm(wx.Dialog):
         #self.stdButtonSizerOK.Bind(wx.EVT_BUTTON, self.OnOKButtonClick)
 
 
+
+
     def OnInitDialog(self, event):
         logging.info('Playgound Dialog Initialized')
 
@@ -245,3 +241,108 @@ class PlaygroundForm(wx.Dialog):
         self.model.ItemDeleted(dv.NullDataViewItem, self.model.ObjectToItem(self.data[0]))
         del(self.data[0])
 
+    def OnFlexGridSizer(self, parent):
+        panel = w.panel(parent)
+        # vertical sizer to split screen
+        box = wx.BoxSizer(wx.VERTICAL)
+        # sizer to display help text
+        # textbox = wx.BoxSizer(wx.HORIZONTAL)
+        helpstr = """
+This is the FlexGrid Sizer demo 
+specify which columns or rows should grow
+grow flexibly in either direction meaning,
+you can specify proportional amounts for child elements
+and specify behaviour in the non flexible direction
+growable row means in the vertical direction
+growable col means in the horizontal direction
+use the proportion argument in the Add method to make cell grow at different amount """
+        sizerheader = wx.StaticText(panel, 0, "Flex Grid Sizer")
+        sizerlabel = wx.StaticText(panel, 0, helpstr.lstrip())
+        sizerheader.SetFont(wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD))
+        sizerlabel.SetFont(wx.Font(14, wx.ROMAN, wx.NORMAL, wx.NORMAL))
+        box.Add(sizerheader, 0, wx.ALL, 5)
+        # add a static line
+        box.Add(wx.StaticLine(panel), 0, wx.EXPAND | wx.TOP | wx.BOTTOM, 5)
+        box.Add(sizerlabel, 0, wx.ALL, 5)
+        box.Add(wx.StaticLine(panel), 0, wx.EXPAND | wx.TOP | wx.BOTTOM, 5)
+
+        # grid bag sizer demo
+        demolabel = wx.StaticText(panel, -1, "Name:")
+        demotext = wx.TextCtrl(panel, -1, "Emmett")
+
+        agelabel = wx.StaticText(panel, -1, "Date of Birth:")
+        agetext = wx.TextCtrl(panel, -1, "06/19/2015")
+
+        lbladdress = wx.StaticText(panel, -1, "Address:")
+        addr1 = wx.TextCtrl(panel, -1, "Flat 1")
+        addr2 = wx.TextCtrl(panel, -1, "18890 Edgewood Ln")
+
+        lblcity = wx.StaticText(panel, -1, "City, State, Zip:")
+        city = wx.TextCtrl(panel, -1, "Prior Lake", size=(150, -1))
+        state = wx.TextCtrl(panel, -1, "MN", size=(50, -1))
+        zip = wx.TextCtrl(panel, -1, "55370", size=(70, -1))
+
+        lblphone = wx.StaticText(panel, -1, "Phone:")
+        phone = wx.TextCtrl(panel, -1, "612 562 4433")
+
+        lblemail = wx.StaticText(panel, -1, "Email:")
+        email = wx.TextCtrl(panel, -1, "mussym@live.com")
+
+        gridsizer = wx.FlexGridSizer(cols=2, hgap=5, vgap=5)
+        gridsizer.AddGrowableCol(1)
+
+        gridsizer.Add(demolabel, proportion=0, flag=wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        # make the name expand
+        gridsizer.Add(demotext, proportion=0, flag=wx.EXPAND)
+
+        # age
+        gridsizer.Add(agelabel, 0, wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        # keep the age a fixed size
+        gridsizer.Add(agetext, 0, 0)
+
+        # address
+        gridsizer.Add(lbladdress, 0, wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        gridsizer.Add(addr1, 0, wx.EXPAND)
+        # empty space in between, address 2 has no label, just space
+        # gridsizer.Add((10, 10))
+        gridsizer.AddSpacer(10)
+        gridsizer.Add(addr2, 0, wx.EXPAND)
+
+        # city state zip
+        gridsizer.Add(lblcity, 0, wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        # an inline box sizer for the single line city, state, zip
+        cstsizer = wx.BoxSizer(wx.HORIZONTAL)
+        cstsizer.Add(city, 1)  # city expands, state and zip do not
+        cstsizer.Add(state, 0, wx.LEFT | wx.RIGHT, 5)  # border on each side of state
+        cstsizer.Add(zip)
+        gridsizer.Add(cstsizer, 0, wx.EXPAND)
+
+        # phone
+        gridsizer.Add(lblphone, 0, wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        gridsizer.Add(phone, 0, wx.EXPAND)
+
+        # email
+        gridsizer.Add(lblemail, 0, wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL)
+        gridsizer.Add(email, 0, wx.EXPAND)
+
+        # can add a sizer to a sizer, not just add widget to sizer, creates a nested sizer
+        box.Add(gridsizer, 0, wx.EXPAND | wx.ALL, 10)
+
+        # some buttons on the bottom
+        savebtn = wx.Button(panel, -1, "Save")
+        cancelbtn = wx.Button(panel, -1, "Cancel")
+        btnSizer = wx.BoxSizer(wx.HORIZONTAL)
+        # this spacer pushes the buttons to the right
+        # btnSizer.Add((20, 20), 1)
+        btnSizer.AddStretchSpacer(20)
+        btnSizer.Add(savebtn)
+        btnSizer.AddSpacer(20)
+        btnSizer.Add(cancelbtn)
+        btnSizer.AddSpacer(20)
+        box.Add(btnSizer, 0, wx.EXPAND | wx.BOTTOM | wx.ALIGN_RIGHT, 10)
+
+        panel.SetSizer(box)
+        box.Fit(parent)  # this call triggers the layout alorithm to fire
+        box.SetSizeHints(parent)
+        panel.SetBackgroundColour("orange")
+        panel.Refresh()
