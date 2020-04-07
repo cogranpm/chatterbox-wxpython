67fe4891562a8ede32b4a1b964d3ab3608c18f25
paul martin <mussym@live.com>
2020-04-07 10:19:46 -0500

    seesaw style ui wrappers

--- forms.py
+++ forms.py
@@ -14,6 +14,7 @@ class FormSpec():
         self.helpstr = helpstr
 
 class FormLineSpec():
+    """ can be made up of multiple edit fields or a single, such as zip, state, city on a single line """
 
     def __init__(self, labelstr: str, edit_fields):
         self.labelstr = labelstr
@@ -21,5 +22,8 @@ class FormLineSpec():
 
 class EditFieldSpec():
 
-    def __init__(self, type: EditFieldType, width: EditFieldWidth):
+    def __init__(self, name: str, caption: str, type: EditFieldType, width: EditFieldWidth):
         self.type = type
+        self.name = name
+        self.caption = caption
+        self.width = width
